577d2f1bc379c2f82ddcb5a007683f6cfaeceecb
budds300 <budds300@gmail.com>
2022-01-12 06:11:25 +0300

    initial init

---
+++ src/Project/ConnectionProvider.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Project;
+import java.sql.*;
+
+/**
+ *
+ * @author Givondo
+ */
+public class ConnectionProvider {
+    public static Connection getCon(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/electronics", "root", "Accessdenied1!");
+            return con;
+                
+        }
+        catch(Exception e){
+            System.out.print(e);
+            return null;
+        }
+    
+}
+}
